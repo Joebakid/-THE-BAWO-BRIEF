dd3fe3f0d5265bc1e7ebde7c8c2550cf78540f94
joebakid <josephbawo@gmail.com>
2025-07-18 16:59:19 +0100

    substack

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -6,18 +6,11 @@ export default function Subscribe() {
         📬 Subscribe to The Bawo Brief
       </h2>
       <form
-        action="https://thebawobrief.substack.com/subscribe"
+        action="https://josephbawo.substack.com/subscribe?next=https%3A%2F%2Fsubstack.com%2F%40thebawobrief&utm_source=profile-page&utm_medium=web&utm_campaign=substack_profile&just_signed_up=true"
         method="post"
         target="_blank"
         className="flex flex-col gap-3"
       >
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter your email"
-          required
-          className="p-2 border border-gray-300 rounded"
-        />
         <button
           type="submit"
           className="px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700"
